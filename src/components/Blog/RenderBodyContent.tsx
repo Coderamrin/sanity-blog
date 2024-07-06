bf0d6f677c2e34ff1ce0f683a685b8cbdc906a6b
coderamrin <coderamrin@gmail.com>
2024-07-06 14:27:20 +0600

    update

---
+++ src/components/Blog/RenderBodyContent.tsx
@@ -0,0 +1,48 @@
+import config from "@/sanity/config/client-config";
+import { Blog } from "@/types/blog";
+import { PortableText } from "@portabletext/react";
+import { getImageDimensions } from "@sanity/asset-utils";
+import urlBuilder from "@sanity/image-url";
+import Image from "next/image";
+
+// lazy-loaded image component
+const ImageComponent = ({ value, isInline }: any) => {
+  const { width, height } = getImageDimensions(value);
+  return (
+    <div className="my-10 overflow-hidden rounded-[15px]">
+      <Image
+        src={
+          urlBuilder(config)
+            .image(value)
+            .fit("max")
+            .auto("format")
+            .url() as string
+        }
+        width={width}
+        height={height}
+        alt={value.alt || "blog image"}
+        loading="lazy"
+        style={{
+          display: isInline ? "inline-block" : "block",
+          aspectRatio: width / height,
+        }}
+      />
+    </div>
+  );
+};
+
+const components = {
+  types: {
+    image: ImageComponent,
+  },
+};
+
+const RenderBodyContent = ({ post }: { post: Blog }) => {
+  return (
+    <>
+      <PortableText value={post?.body as any} components={components} />
+    </>
+  );
+};
+
+export default RenderBodyContent;
